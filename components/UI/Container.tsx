d225fe7ff60d6ebf1238ef99827e6d9750146f3e
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-02-28 21:30:23 +0600

    Container UI Lib

---
+++ components/UI/Container.tsx
@@ -0,0 +1,102 @@
+import {HTMLAttributes} from "react";
+import classNames from "classnames";
+
+interface ContainerInterface extends HTMLAttributes<HTMLDivElement> {
+    overWrite?: boolean,
+}
+
+export const Container = ({className, children, overWrite = false, ...props}: ContainerInterface) => {
+    return (
+        <div className={classNames(!overWrite && "container mx-auto", className)} {...props}>
+            {children}
+        </div>
+    )
+}
+
+export const Section = (props: HTMLAttributes<HTMLElement>) => {
+    return (
+        <section {...props}>
+            {props.children}
+        </section>
+    )
+}
+
+export const Div = (props: HTMLAttributes<HTMLDivElement>) => {
+    return (
+        <div {...props}>
+            {props.children}
+        </div>
+    )
+}
+
+export const Grid = ({className, children, ...props}: HTMLAttributes<HTMLDivElement>) => {
+    return (
+        <Div className={classNames("grid", className)} {...props}>
+            {children}
+        </Div>
+    )
+}
+
+export const Flex = ({className, children, ...props}: HTMLAttributes<HTMLDivElement>) => {
+    return (
+        <Div className={classNames("flex", className)} {...props}>
+            {children}
+        </Div>
+    )
+}
+
+export const FlexRow = ({className, children, ...props}: HTMLAttributes<HTMLDivElement>) => {
+    return (
+        <Flex className={classNames("flex-row", className)} {...props}>
+            {children}
+        </Flex>
+    )
+}
+
+export const Row = ({className, children, ...props}: HTMLAttributes<HTMLDivElement>) => {
+    return (
+        <Div className={classNames(className)} {...props}>
+            {children}
+        </Div>
+    )
+}
+
+export const Col = ({className, children, ...props}: HTMLAttributes<HTMLDivElement>) => {
+    return (
+        <Div className={classNames(className)} {...props}>
+            {children}
+        </Div>
+    )
+}
+
+export const FlexCol = ({className, children, ...props}: HTMLAttributes<HTMLDivElement>) => {
+    return (
+        <Flex className={classNames("flex-col", className)} {...props}>
+            {children}
+        </Flex>
+    )
+}
+
+export const FlexRowWrap = ({className, children, ...props}: HTMLAttributes<HTMLDivElement>) => {
+    return (
+        <Flex className={classNames("flex-row flex-wrap", className)} {...props}>
+            {children}
+        </Flex>
+    )
+}
+
+export const Relative = ({className, children, ...props}: HTMLAttributes<HTMLDivElement>) => {
+    return (
+        <Div className={classNames("relative", className)} {...props}>
+            {children}
+        </Div>
+    )
+}
+
+export const Absolute = ({className, children, ...props}: HTMLAttributes<HTMLDivElement>) => {
+    return (
+        <Div className={classNames("absolute", className)} {...props}>
+            {children}
+        </Div>
+    )
+}

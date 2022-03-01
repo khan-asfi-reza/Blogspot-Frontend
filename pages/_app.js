e85026d73850a47b20bed229e300003f49e0167c
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-03-01 18:15:52 +0600

    Framer Motion In App File

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
+import {AnimatePresence} from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <AnimatePresence exitBeforeEnter={true}>
+    <Component {...pageProps}/>
+  </AnimatePresence>
 }
 
 export default MyApp

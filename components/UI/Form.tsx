818855b47e1ad661361c7492e2ec98eacf7669c4
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-04-04 00:57:40 +0600

    Form button fix

--- components/UI/Form.tsx
+++ components/UI/Form.tsx
@@ -1,27 +1,24 @@
-import {HTMLAttributes, InputHTMLAttributes} from "react";
+import {forwardRef, HTMLAttributes, InputHTMLAttributes} from "react";
 import classNames from "classnames";
 
 export const Form = (props: HTMLAttributes<HTMLFormElement>) => {
-    return (
-        <form {...props}>
-            {props.children}
-        </form>
-    )
+    return (<form {...props}>
+        {props.children}
+    </form>)
 }
 
-export const InputGroup = (props: HTMLAttributes<HTMLDivElement>) => (
-    <div {...props}>
-        {props.children}
-    </div>
-)
+export const InputGroup = (props: HTMLAttributes<HTMLDivElement>) => (<div {...props}>
+    {props.children}
+</div>)
 
-export const Input = (props: InputHTMLAttributes<HTMLInputElement>) => (
-    <input  {...props} className={classNames(props.className, "active:outline-0 outline-0 px-2 focus:outline-0")}/>
 
+export const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>(
+    (props, ref) => (
+        <input ref={ref} {...props}
+               className={classNames(props.className, "active:outline-0 outline-0 px-2 focus:outline-0")}/>)
 )
+Input.displayName = "Input"
 
-export const Label = (props: HTMLAttributes<HTMLLabelElement>) => (
-    <label {...props}>
-        {props.children}
-    </label>
-)
+export const Label = (props: HTMLAttributes<HTMLLabelElement>) => (<label {...props}>
+    {props.children}
+</label>)

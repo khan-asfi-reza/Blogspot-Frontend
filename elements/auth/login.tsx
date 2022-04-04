7bc91c8d67283a890aec63c922156ab280423df6
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-04-04 22:37:15 +0600

    Login section structure update

--- elements/auth/login.tsx
+++ elements/auth/login.tsx
@@ -1,78 +1,64 @@
-import {Div, Grid} from "../../components/UI/Container";
+import {Div} from "../../components/UI/Container";
 import {Anchor} from "../../components/UI/Button";
-import {useState} from "react";
-import {IoAtCircleOutline, IoLockClosedOutline, IoLockOpenOutline} from "react-icons/io5";
+import {IoAtCircleOutline} from "react-icons/io5";
 import useApi from "../../hook/useApi";
-import {AuthForm, AuthFormFooter, AuthFormHeader} from "./components";
+import {AuthForm, AuthFormContainer, AuthFormFieldInterface, AuthFormHeader, passwordTextSwitch} from "./components";
 
-export default function Login() {
+export default function Login({state, setState}) {
 
-    const [formData, setState] = useState({
-        email: "",
-        password: "",
-    });
-
-    const setInputState = (e) => {
-        setState({
-            ...formData,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    const InputFields = [
+    const InputFields: Array<AuthFormFieldInterface> = [
         {
-            name: "email",
-            type: "text",
-            label: "Email",
-            value: formData.email,
-            icon: <IoAtCircleOutline/>,
-            row: [],
-            rowClass: "",
+            field: {
+                icon: <IoAtCircleOutline/>,
+                inputProps: {
+                    type: "text",
+                    name: "email",
+                    onChange: setState,
+                    value: state.email
+                },
+                label: "Email",
+                validator: () => {
+                    if (state.email === "test1234@gmail.com") {
+                        return "Invalid email address"
+                    }
+                    return false
+                },
+            }
         },
         {
-            name: "password",
-            type: "password",
-            label: "Password",
-            value: formData.password,
-            icon: <IoLockOpenOutline/>,
-            row: [],
-            rowClass: "",
-            iconOnClick: function (ref, changeIcon) {
-                if (ref.current.type === "text") {
-                    ref.current.type = "password";
-                    changeIcon(<IoLockClosedOutline/>)
-                } else {
-                    ref.current.type = "text";
-                    changeIcon(<IoLockOpenOutline/>)
-                }
+            field: {
+                icon: <IoAtCircleOutline/>,
+                inputProps: {
+                    type: "password",
+                    name: "password",
+                    onChange: setState,
+                    value: state.password
+                },
+                label: "Password",
+                iconOnClick: passwordTextSwitch
             }
-        }
+        },
     ]
 
     const [apiData, postApi] = useApi<{ email: string, password: string }>('/api/auth/login',);
 
     const formOnClick = (e) => {
         e.preventDefault();
-        postApi(formData).then(() => {
-            console.log("HELLO")
-        });
+        postApi(state)
+            .then(() => {
+                console.log(apiData)
+            })
+
     }
 
     return (
-        <Grid className={"grid  w-full h-full relative"}>
-            <Div className={"w-full"}>
-                <AuthFormHeader text={"Login to Blogspot"}/>
-                <AuthForm loading={apiData.loading}
-                          setInputState={setInputState}
-                          formOnSubmit={formOnClick}
-                          fields={InputFields}>
-                    <Div className={"text-right"}>
-                        <Anchor className={"mt-4 hover:text-yellow-700"}>Forgot Password?</Anchor>
-                    </Div>
-                </AuthForm>
-                <AuthFormFooter text={"Don't have an account?"} buttonText={"Create Account"} onClick={() => {
-                }}/>
-            </Div>
-        </Grid>
+        <AuthFormContainer>
+            <AuthFormHeader text={"Login to Lightboat"}/>
+            <AuthForm loading={apiData.loading} formOnSubmit={formOnClick} fields={InputFields}>
+                <Div className={"text-right"}>
+                    <Anchor className={"mt-4 hover:text-yellow-700"}>Forgot Password?</Anchor>
+                </Div>
+            </AuthForm>
+        </AuthFormContainer>
     )
 }

6bcbe31232fac242291b1dd9f5cc7de6716322f0
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-04-04 22:35:56 +0600

    New Components added for auth page

--- elements/auth/components.tsx
+++ elements/auth/components.tsx
@@ -1,14 +1,73 @@
-import {Div, FlexRow, Grid} from "../../components/UI/Container";
+import {Col, Div, Grid} from "../../components/UI/Container";
 import Image from "next/image";
 import Logo from "../../assets/images/logo.png";
 import {SectionHeading, SmallText} from "../../components/UI/Typography";
-import {Form, Input, InputGroup, Label} from "../../components/UI/Form";
+import {Form, Input, InputInterface} from "../../components/UI/Form";
 import classNames from "classnames";
 import {FaSpinner} from "react-icons/fa";
 import {Button} from "../../components/UI/Button";
-import {useRef, useState} from "react";
+import {FormEventHandler, MouseEventHandler} from "react";
+import {IoLockClosedOutline, IoLockOpenOutline} from "react-icons/io5";
+import LoginPageImage from "../../assets/images/login.png";
 
 
+export function passwordTextSwitch(ref, changeIcon) {
+    if (ref.current.type === "text") {
+        ref.current.type = "password";
+        changeIcon(<IoLockClosedOutline/>)
+    } else {
+        ref.current.type = "text";
+        changeIcon(<IoLockOpenOutline/>)
+    }
+}
+
+
+export function AuthFormSideBar() {
+    return (
+        <Col
+            className={"md:static relative md:h-auto bg-theme h-56 w-full md:rounded-none rounded-t-3xl md:p-12 grid place-items-center z-20"}>
+            <Div>
+                <Div
+                    className={"md:static absolute top-12 left-1/2 md:translate-x-0 -translate-x-1/2 md:h-auto md:w-full md:border-0 border-8 border-theme h-56 w-56 rounded-full md:bg-transparent bg-blue-100 grid place-items-center mx-auto"}>
+                    <Div className={"md:w-full mx-auto w-10/12 "}>
+                        <Image alt={"LoginImage"} src={LoginPageImage}/>
+                    </Div>
+                </Div>
+                <Div className={"md:grid hidden place-items-center"}>
+                    <Div>
+                        <SectionHeading
+                            className={"text-center text-white font-medium"}>
+                            Read write and enjoy
+                        </SectionHeading>
+                        <SmallText className={"text-gray-100" +
+                            " text-center font-base"}>
+                            Lightboat the finest premium blogging site
+                        </SmallText>
+                    </Div>
+                </Div>
+            </Div>
+        </Col>
+    )
+}
+
+
+// Container Of Auth Form
+export function AuthFormContainer({children}) {
+    return (
+        <Grid className={"grid w-full h-full relative flex-1"}>
+            <Div className={"w-full"}>
+                {children}
+            </Div>
+        </Grid>
+    )
+}
+
+
+// Authentication Form Header Images and Text along with logo
+/**
+ *
+ * @param text - Small Text below Welcome Message
+ */
 export function AuthFormHeader({text}: { text: string }) {
     return (
         <Div>
@@ -29,7 +88,18 @@ export function AuthFormHeader({text}: { text: string }) {
     )
 }
 
-export function AuthFormFooter({text, onClick, buttonText}) {
+/**
+ *
+ * @param text - Footer Text
+ * @param onClick - Footer Button On Click Event
+ * @param buttonText - Footer Button Text
+ */
+export function AuthFormFooter({text, onClick, buttonText}:
+                                   {
+                                       text: string,
+                                       onClick: MouseEventHandler<HTMLSpanElement>,
+                                       buttonText: string
+                                   }) {
     return (
         <>
             <Div className={"mt-10"}>
@@ -46,74 +116,50 @@ export function AuthFormFooter({text, onClick, buttonText}) {
     )
 }
 
-export function AuthInputGroup({setInputState, value, name, type, icon, label, iconOnClick = null}) {
-    const inputRef = useRef(null);
-    const [currentIcon, changeCurrentIcon] = useState(icon);
-
-    return (
-        <InputGroup className={"group mt-6"}>
-            <FlexRow
-                className={"relative rounded-lg bg-gray-50 group border group-focus-within:border-emerald-500 transition-all duration-500 "}>
-                <Input onChange={setInputState}
-                       value={value}
-                       name={name}
-                       ref={inputRef}
-                       type={type}
-                       required={true}
-                       className={"text-gray-700 autofill-transparent peer bg-transparent transition-all duration-500 w-full h-12"}/>
-                <Label
-                    className={classNames(
-                        "absolute  pointer-events-none top-1/2 ",
-                        "peer-focus:top-0 peer-focus:before:bg-gray-50 before:h-[60%] before:absolute before:w-full before:bottom-0 before:-z-10 ",
-                        "transform left-2  peer-valid:top-0 peer-valid:before:bg-gray-50 bg-transparent -translate-y-1/2 text-md group-focus-within:text-emerald-600 ",
-                        "transition-all duration-150"
-                    )}>
-                    {label}
-                </Label>
-                <span
-                    onClick={() => {
-                        if (iconOnClick) {
-                            iconOnClick(inputRef, changeCurrentIcon)
-                        }
-                    }}
-                    className={"text-2xl peer-focus:text-emerald-500 grid place-items-center px-2"}>
-                    {currentIcon}
-                </span>
-            </FlexRow>
-        </InputGroup>
-    )
+/**
+ * Interface for List of Form Fields
+ */
+export interface AuthFormFieldInterface {
+    field: InputInterface,
+    row?: Array<InputInterface>,
+    rowClass?: string
 }
 
-export function AuthForm({fields, setInputState, formOnSubmit, children, loading}) {
+
+interface AuthFormPropsInterface {
+    fields: Array<AuthFormFieldInterface>,
+    loading: boolean,
+    formOnSubmit: FormEventHandler<HTMLFormElement>,
+    children?: JSX.Element
+}
+
+/**
+ * Authentication Page Form For Signing in and Signing up
+ * @param {Array<AuthFormFieldInterface>}fields - List of Input Fields inside the form
+ * @param {FormEventHandler<HTMLFormElement>}formOnSubmit - Form On submit action
+ * @param {JSX.Element}children - JSX Children
+ * @param {boolean}loading - Is Button Loading or not
+ *
+ */
+export function AuthForm({fields, formOnSubmit, children, loading}: AuthFormPropsInterface) {
+
     return (
         <Form onSubmit={formOnSubmit} className={"mt-10"}>
             {
-                fields.map(({name, type, label, value, icon, row, rowClass, iconOnClick}, key) =>
-                    row.length ? <Grid className={classNames(rowClass, "gap-x-6")}>
-                            {row.map((rowData, k) => (
-                                <AuthInputGroup
-                                    key={k}
-                                    setInputState={setInputState}
-                                    value={rowData.value}
-                                    name={rowData.name}
-                                    type={rowData.type}
-                                    icon={rowData.icon}
-                                    label={rowData.label}/>
-                            ))}
+                fields.map((props, key) =>
+                    props.row && props.row.length ?
+                        <Grid key={key} className={classNames(props.rowClass, "gap-x-6")}>
+                            {
+                                props.row.map((each, k) => (
+                                    <Input key={k} {...each}/>
+                                ))
+                            }
                         </Grid> :
-                        <AuthInputGroup setInputState={setInputState}
-                                        key={key}
-                                        iconOnClick={iconOnClick}
-                                        value={value}
-                                        name={name}
-                                        type={type}
-                                        icon={icon}
-                                        label={label}/>
+                        <Input key={key} {...props.field}/>
                 )
             }
             {children}
-            <Button disabled={loading}
-                    type={"submit"}
+            <Button disabled={loading} type={"submit"}
                     className={classNames("bg-theme justify-center w-full flex items-center mt-12",
                         "text-white disabled:bg-emerald-500 md:px-10 px-8 hover:bg-blue-500 transition-all")}>
                 {loading && <i className={"animate-spin mr-2"}><FaSpinner/></i>}

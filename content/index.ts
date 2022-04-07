a567b09b8d0fb85fded6351f3c7670973a573133
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-04-07 18:45:15 +0600

    More Slides Added

--- content/index.ts
+++ content/index.ts
@@ -1,3 +1,8 @@
+import LoginImage from "../assets/images/login.png";
+import LoginImage2 from "../assets/images/login_2.png";
+import LoginImage3 from "../assets/images/login_3.png";
+import LoginImage4 from "../assets/images/login_4.png";
+
 // Global
 export const BRAND_NAME = "Firebolt";
 // Auth Page
@@ -11,3 +16,26 @@ export const SWITCH_TO_LOGIN_TEXT = "Already have an account?"
 export const SWITCH_TO_LOGIN_BUTTON_TEXT = "Sign In";
 export const AUTH_SIDE_BAR_HEADING = "Read, Write and excel";
 export const AUTH_SIDE_BAR_TEXT = "Welcome to the finest blog media";
+
+export const AUTH_FORM_SIDE_SECTION_CONTENTS = [
+    {
+        title: "Read, Write and excel",
+        text: "Welcome to the finest blog media",
+        image: LoginImage
+    },
+    {
+        title: "Share your awesome thoughts",
+        text: "Share your thoughts and hear from others",
+        image: LoginImage2
+    },
+    {
+        title: "Build a community",
+        text: "Write, share, post and create a community",
+        image: LoginImage3
+    },
+    {
+        title: "Make friends and enjoy",
+        text: "Make friends and share thoughts",
+        image: LoginImage4
+    }
+]

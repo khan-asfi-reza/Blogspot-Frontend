8d01defad15f0a355b648daf83fbc0bca791fa5c
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-04-08 20:14:30 +0600

    Index page layout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-export default function Home() {
-  return (
-    <>
+import {Navigation} from "../components/UI/Navigation";
 
-    </>
-  )
+export default function Home() {
+    return (
+        <main className={"bg-gray-100 min-h-screen"}>
+            <Navigation/>
+        </main>
+    )
 }

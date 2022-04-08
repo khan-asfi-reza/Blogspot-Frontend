29d71cc67eee4aba0fb32267812cf62e36467991
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-04-08 12:35:45 +0600

    Config Edit

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,9 @@ module.exports = {
     ],
     moduleNameMapper: {
         "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
-        "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+        "^.+\\.(css|sass|scss)$": "<rootDir>/test/__mocks__/styleMock.js",
         "^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$":
-            "<rootDir>/__mocks__/fileMock.js",
+            "<rootDir>/test/__mocks__/fileMock.js",
     },
     testPathIgnorePatterns: [
         "<rootDir>/node_modules/",

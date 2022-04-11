c1f22b46cf707aa8b3805ab42b7609c773379260
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-04-11 13:53:16 +0600

    Styleguide config edit

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,13 +1,38 @@
+const path = require("path");
+const sections = require("./section.js");
+
 module.exports = {
-    components: "./components/**/*.tsx",
     propsParser: require("react-docgen-typescript").parse,
-    sections: [
-        {
-            name: "UI Components",
-            components: "./components/UI/Container.tsx",
-            content: 'docs/Container.md',
-            exampleMode: 'expand', // 'hide' | 'collapse' | 'expand'
-            usageMode: 'expand' // 'hide' | 'collapse' | 'expand'
+    resolver: require("react-docgen").resolver.findAllComponentDefinitions,
+    theme: {
+        fontFamily: {
+            base: '"Poppins", sans-serif',
+            monospace: ["Fira Code"],
         }
-    ]
+    },
+    getExampleFilename(componentPath) {
+        const name = path.basename(componentPath, '.tsx')
+        return `docs/components/${name.replace(/\.tsx$/, '.md')}`
+    },
+    getComponentPathLine(componentPath) {
+        const name = path.basename(componentPath, '.tsx')
+        let dir = path.dirname(componentPath)
+        if (dir.includes('src\\components\\UI')) {
+            dir = dir.replace('src\\components\\UI', "@UI")
+        }
+        dir = dir.replace(/\\/g, '/');
+        return `import ${name} from "${dir}";`
+    },
+    contextDependencies: [
+        path.resolve(__dirname, "section.js")
+    ],
+    components: "src/components/**/*.js",
+    require: [path.join(__dirname, 'docs/dist/dist.css')],
+    moduleAliases: {
+        "@UI": path.resolve(__dirname, "src/components/UI/"),
+    },
+    styleguideComponents: {
+        Wrapper: path.join(__dirname, 'src/containers/styleguide/Wrapper')
+    },
+    sections: sections
 };

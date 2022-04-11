7c016c010310ff3bab4c2b8b9e08df2fa728ecf9
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-04-11 13:54:50 +0600

    Documentation section

---
+++ section.js
@@ -0,0 +1,19 @@
+module.exports = [
+    {
+        name: "UI Layout",
+        sections: [
+            {
+                name: "Container",
+                components: "src/components/UI/Layout/Container.tsx",
+                content: "docs/components/Container.md",
+                exampleMode: 'expand',
+            },
+            {
+                name: "Layouts",
+                components: "src/components/UI/Layout/Layout.tsx",
+                content: "docs/components/Layout.md",
+                exampleMode: 'expand',
+            }
+        ]
+    }
+]

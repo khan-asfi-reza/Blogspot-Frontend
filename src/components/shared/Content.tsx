68a5debbc9757e43decfeb205bb116aede55ef6b
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-04-22 23:56:32 +0600

    Content Design Changes

--- src/components/shared/Content.tsx
+++ src/components/shared/Content.tsx
@@ -1,7 +1,8 @@
 import Image, {StaticImageData} from "next/image";
-import moment from "moment";
-import {IoStarOutline} from "react-icons/all";
+import {IoChatbubbleOutline, IoStarOutline} from "react-icons/io5";
 import classNames from "classnames";
+import Moment from "@SharedComponents/Moment";
+import Number from "@SharedComponents/Number";
 
 export interface ContentProps {
     profilePicture: StaticImageData | string,
@@ -15,16 +16,6 @@ export interface ContentProps {
 
 export default function Content(props: ContentProps) {
 
-    const formatDate = () => {
-        const now = new Date();
-        const providedDate = new Date(props.date);
-        const diff = Math.abs(providedDate.getTime() - now.getTime()) / (60 * 60 * 1000);
-        if (diff < 24) {
-            return moment(props.date).startOf('day').fromNow();
-        } else {
-            return moment(props.date).format('MMMM Do YYYY');
-        }
-    }
 
     return (
         <div className={"grid gap-4 bg-white p-8 rounded-lg"}>
@@ -37,7 +28,7 @@ export default function Content(props: ContentProps) {
                         {props.name}
                     </p>
                     <p className={"text-gray-600 text-sm"}>
-                        {formatDate()}
+                        <Moment date={props.date}/>
                     </p>
                 </div>
             </div>
@@ -53,28 +44,29 @@ export default function Content(props: ContentProps) {
             </div>
             <div className={"grid grid-cols-2"}>
                 <div className={"flex items-center gap-x-2"}>
-                    <button className={classNames("gap-x-2 transition-all rounded-full",
-                        "flex items-center px-6 py-2 hover:bg-gray-200 bg-slate-100 text-xl")}>
-                        <span className={"text-gray-700"}>
-                            <IoStarOutline/>
-                        </span>
-                    </button>
-
-                </div>
-                <div className={"flex gap-x-4 justify-end"}>
-                    <button className={classNames("gap-x-2 transition-all rounded-full",
-                        "flex items-center px-6 py-2 hover:bg-gray-200 bg-slate-100 text-xl")}>
-                        <span className={"text-blue-500"}>
-                            <IoStarOutline/>
-                        </span>
-                        <span className={"text-md text-gray-700 "}>
+                    <button
+                        className={classNames("text-gray-700 hover:bg-gray-200  gap-x-2 transition-all rounded-full",
+                            "flex items-center px-6 py-2 bg-slate-100 text-xl")}>
+                        <IoStarOutline/>
+                        <span className={"text-md "}>
                             3.5k
                         </span>
                     </button>
+
+                    <button
+                        className={classNames("text-gray-700 hover:bg-gray-200  gap-x-2 transition-all rounded-full",
+                            "flex items-center px-6 py-2 bg-slate-100 text-xl")}>
+                        <IoChatbubbleOutline/>
+                        <span className={"text-md "}>
+                            <Number number={3000}/>
+                        </span>
+                    </button>
+                </div>
+                <div className={"flex gap-x-4 justify-end"}>
                     {
                         props.tags.map((tag, key) => (
                             <div key={key}
-                                 className={"bg-slate-100 grid place-items-center text-gray-700 px-4 py-1 rounded-full text-sm"}>
+                                 className={"bg-gray-200 grid place-items-center text-gray-700 px-4 py-1 rounded-full text-sm"}>
                                 <p>
                                     {tag}
                                 </p>

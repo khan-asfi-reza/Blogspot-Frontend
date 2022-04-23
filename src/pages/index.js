1067ff14f50f7d7bee9a36709591819e3710fbd6
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-04-24 03:23:08 +0600

    Adding new elements to the test index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,23 @@ import {Navigation} from "../components/shared/Navigation";
 import Person from "../assets/images/person.jpg";
 import Code from "../assets/images/code.jpg";
 import Content from "../components/shared/Content";
-
+import {SecondaryInput} from "../components/UI/Form";
+import {IoSearchOutline} from "react-icons/io5";
+import {useState} from "react";
+import Image from "next/image";
+import {AnimatePresence} from "framer-motion";
+import {Div} from "../components/UI/Layout";
 
 export default function Home() {
+
+    const [searchOpen, setSearchOpen] = useState(false);
+
     return (
         <main className={"bg-gray-100 min-h-screen"}>
             <Navigation/>
             <section className={"pt-12 container max-w-7xl mx-auto"}>
-                <div className={"grid grid-cols-3 gap-20"}>
-                    <div className={"grid gap-10 col-span-2"}>
+                <div className={"grid lg:grid-cols-3 md: gap-20"}>
+                    <div className={"grid gap-10 md:col-span-2"}>
                         <Content name={"David Silva"}
                                  title={"Why not to use PHP"}
                                  tags={["React"]}
@@ -30,6 +38,111 @@ export default function Home() {
                                      "Once upon a time I was doing some Symfony2 myself. And I liked the whole Symfony thing, frankly, it’s not bad. I told this to a developer I met at a devops meetup. He was shocked, he was about to vomit, he looked at me like I just murdered a newborn baby. From now on I’m going to call him James to make it easier. So I immediately asked James a question: why so much hatred?"}
                                  image={Code}
                                  profilePicture={Person}/>
+                    </div>
+                    <div className={"flex flex-col gap-y-10"}>
+                        <div className={"relative z-30"}>
+                            <SecondaryInput containerProps={{className: "bg-[#fff]"}}
+                                            icon={<IoSearchOutline/>}
+                                            label={"Search anything"}
+                                            inputProps={{
+                                                onFocus: () => {
+                                                    setSearchOpen(true);
+                                                },
+                                                onBlur: () => {
+                                                    setSearchOpen(false);
+                                                }
+                                            }}
+                            >
+
+                            </SecondaryInput>
+                            <AnimatePresence exitBeforeEnter>
+                                {
+                                    searchOpen &&
+                                    <Div initial={{opacity: 0, scale: 0.95}}
+                                         animate={{opacity: 1, scale: 1}}
+                                         exit={{opacity: 0, scale: 0.95}}
+                                         className={"absolute grid gap-y-4 top-[110%] outline outline-gray-100 ring-1 bg-white w-full z-10 rounded-lg py-4 border border-gray-100"}>
+                                        <button
+                                            className={"flex group items-center  gap-x-4 px-4 hover:bg-theme py-1 rounded-lg"}>
+                                            <div className={"h-10 w-10 rounded-full overflow-hidden relative"}>
+                                                <Image src={Person} alt={""} layout={"fill"} objectFit={"cover"}/>
+                                            </div>
+                                            <div>
+                                                <p className={"text-md text-gray-800 group-hover:text-gray-50"}>
+                                                    David Silva
+                                                </p>
+                                            </div>
+                                        </button>
+
+                                        <button
+                                            className={"flex group items-center gap-x-4 px-4 hover:bg-theme py-1 rounded-lg"}>
+                                            <div className={"h-10 w-10 rounded-full overflow-hidden relative"}>
+                                                <Image src={Person} alt={""} layout={"fill"} objectFit={"cover"}/>
+                                            </div>
+                                            <div>
+                                                <p className={"text-md text-gray-800 group-hover:text-gray-50"}>
+                                                    David Silva
+                                                </p>
+                                            </div>
+                                        </button>
+                                    </Div>
+                                }
+                            </AnimatePresence>
+                        </div>
+
+                        <div className={"w-full bg-white rounded-xl grid gap-4 py-4"}>
+                            <h2 className={"text-xl font-bold text-black px-4"}>
+                                Trends
+                            </h2>
+
+                            <div className={"grid"}>
+                                <div className={"border-b border-slate-200 py-4 px-4 hover:bg-slate-200"}>
+                                    <p className={"text-gray-600 text-xs"}>
+                                        Trending #1
+                                    </p>
+                                    <div className={"flex justify-between flex-row "}>
+                                        <p className={"text-gray-700 text-base font-medium"}>
+                                            @React
+                                        </p>
+                                        <p className={"text-gray-600 text-md"}>
+                                            3.5k Blogs
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div className={"border-b border-slate-200 py-4 px-4 hover:bg-slate-200"}>
+                                    <p className={"text-gray-600 text-xs"}>
+                                        Trending #2
+                                    </p>
+                                    <div className={"flex justify-between flex-row "}>
+                                        <p className={"text-gray-700 text-base font-medium"}>
+                                            @React
+                                        </p>
+                                        <p className={"text-gray-600 text-md"}>
+                                            3.5k Blogs
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div className={"border-b border-slate-200 py-4 px-4 hover:bg-slate-200"}>
+                                    <p className={"text-gray-600 text-xs"}>
+                                        Trending #3
+                                    </p>
+                                    <div className={"flex justify-between flex-row "}>
+                                        <p className={"text-gray-700 text-base font-medium"}>
+                                            @React
+                                        </p>
+                                        <p className={"text-gray-600 text-md"}>
+                                            3.5k Blogs
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button className={"text-left px-4 text-sm "}>
+                                Show More
+                            </button>
+                        </div>
                     </div>
                 </div>
             </section>

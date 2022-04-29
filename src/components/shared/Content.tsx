e6345cd6c0c2ca2cc8acbaca3faa282a72382aef
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-04-30 03:06:19 +0600

    Content Design Change

--- src/components/shared/Content.tsx
+++ src/components/shared/Content.tsx
@@ -1,11 +1,10 @@
 import Image, {StaticImageData} from "next/image";
-import {IoChatbubbleOutline, IoStarOutline} from "react-icons/io5";
-import classNames from "classnames";
+import {IoChatbubbleEllipsesOutline, IoEllipsisVertical, IoHeartOutline} from "react-icons/io5";
 import Moment from "@SharedComponents/Moment";
 import Number from "@SharedComponents/Number";
 
 export interface ContentProps {
-    profilePicture: StaticImageData | string,
+    avatar: StaticImageData | string,
     name: string,
     date: Date,
     title: string,
@@ -18,63 +17,51 @@ export default function Content(props: ContentProps) {
 
 
     return (
-        <div className={"grid gap-4 bg-white p-8 rounded-lg"}>
-            <div className={"flex items-center gap-x-2"}>
-                <div className={"h-10 w-10 rounded-full relative overflow-hidden"}>
-                    <Image alt={""} src={props.profilePicture} layout={"fill"} objectFit={"cover"}/>
-                </div>
-                <div className={"flex flex-col"}>
-                    <p className={"font-medium text-md"}>
-                        {props.name}
-                    </p>
-                    <p className={"text-gray-600 text-sm"}>
-                        <Moment date={props.date}/>
-                    </p>
-                </div>
+        <div className={"grid grid-cols-2 gap-6 bg-content sm:p-8 p-4 rounded-lg"}>
+            <div
+                className={"xl:order-1  xl:col-span-1 order-3 col-span-2 xl:h-auto h-60 row-span-4 relative rounded-xl overflow-hidden"}>
+                <Image src={props.image} objectFit={"cover"} layout={"fill"} alt={"Image"}/>
             </div>
-            <h2 className={"font-medium sm:text-2xl"}>
+            <div className={"xl:order-2 xl:col-span-1 col-span-2 order-1 flex  items-center justify-between"}>
+                <div className={"flex items-center gap-x-3"}>
+                    <div className={"h-12 w-12 relative rounded-full overflow-hidden"}>
+                        <Image src={props.avatar} objectFit={"cover"} layout={"fill"} alt={"Avatar"}/>
+                    </div>
+                    <div>
+                        <p className={"text-title text-md"}>
+                            {props.name}
+                        </p>
+                        <p className={"text-subtitle text-xs"}>
+                            <Moment date={props.date}/>
+                        </p>
+                    </div>
+                </div>
+                <button>
+                    <IoEllipsisVertical/>
+                </button>
+            </div>
+            <h3 className={"xl:order-3 xl:col-span-1 col-span-2 order-2 text-title font-medium text-xl"}>
                 {props.title}
-            </h2>
-            <p className={"text-md text-gray-700"}>
-                {props.text.slice(0, 500)}
-                ...
+            </h3>
+            <p className={"xl:order-4 xl:col-span-1 col-span-2 order-4 text-subtext text-md"}>
+                {props.text.slice(0, 150)}
             </p>
-            <div className={"h-96 w-full relative rounded-lg overflow-hidden"}>
-                <Image src={props.image} objectFit={"cover"} layout={"fill"} alt={""}/>
-            </div>
-            <div className={"grid grid-cols-2"}>
-                <div className={"flex items-center gap-x-2"}>
-                    <button
-                        className={classNames("text-gray-700 hover:bg-gray-200  gap-x-2 transition-all rounded-full",
-                            "flex items-center px-6 py-2 bg-slate-100 text-xl")}>
-                        <IoStarOutline/>
-                        <span className={"text-md "}>
-                            3.5k
-                        </span>
-                    </button>
 
-                    <button
-                        className={classNames("text-gray-700 hover:bg-gray-200  gap-x-2 transition-all rounded-full",
-                            "flex items-center px-6 py-2 bg-slate-100 text-xl")}>
-                        <IoChatbubbleOutline/>
-                        <span className={"text-md "}>
-                            <Number number={3000}/>
+            <div className={"xl:order-5 xl:col-span-1 col-span-2 order-5 flex gap-x-8 items-center"}>
+                <p className={"flex items-center gap-x-1 text-rose-500"}>
+                    <IoHeartOutline/>
+                    <span>
+                            <Number number={2050}/>
                         </span>
-                    </button>
-                </div>
-                <div className={"flex gap-x-4 justify-end"}>
-                    {
-                        props.tags.map((tag, key) => (
-                            <div key={key}
-                                 className={"bg-gray-200 grid place-items-center text-gray-700 px-4 py-1 rounded-full text-sm"}>
-                                <p>
-                                    {tag}
-                                </p>
-                            </div>
-                        ))
-                    }
-                </div>
+                </p>
+                <p className={"flex items-center gap-x-1 text-indigo-500"}>
+                    <IoChatbubbleEllipsesOutline/>
+                    <span>
+                            <Number number={2050}/>
+                        </span>
+                </p>
             </div>
+
         </div>
     )
 }

b1fdf1ceed5c5bb32e00ea76cbbb05ac5511a4f1
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-05-03 03:29:15 +0600

    Component Testing Page

---
+++ src/pages/test.tsx
@@ -0,0 +1,16 @@
+import {P} from "@UI/Typography";
+
+export default function TestPage(){
+    return(
+        <>
+            <P size={{
+               '2xl': '7xl',
+               'xl': '6xl',
+                'lg': '6xl',
+                'md': 'base'
+            }}>
+                TESTING
+            </P>
+        </>
+    )
+}

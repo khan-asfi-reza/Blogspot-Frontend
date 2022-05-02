318497effb4dded717ede8afe3c552533b106d59
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-05-03 03:55:37 +0600

    Test Page

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -3,12 +3,7 @@ import {P} from "@UI/Typography";
 export default function TestPage(){
     return(
         <>
-            <P size={{
-               '2xl': '7xl',
-               'xl': '6xl',
-                'lg': '6xl',
-                'md': 'base'
-            }}>
+            <P size={'5xl'}>
                 TESTING
             </P>
         </>

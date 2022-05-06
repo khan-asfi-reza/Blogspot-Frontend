4808af74321e8cffb6fa615e6a9f2ad9cc2f482a
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-05-06 11:00:13 +0600

    Hydrate state

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,20 @@
 import '../styles/globals.css'
 import {AnimatePresence} from "framer-motion";
-import {QueryClient, QueryClientProvider} from "react-query";
+import {Hydrate, QueryClient, QueryClientProvider} from "react-query";
 
 const queryClient = new QueryClient()
+
 
 function App({Component, pageProps}) {
     return (
         <QueryClientProvider client={queryClient}>
-            <AnimatePresence exitBeforeEnter={true}>
-                <Component {...pageProps}/>
-            </AnimatePresence>
+            <Hydrate>
+                <AnimatePresence exitBeforeEnter={true}>
+                    <Component {...pageProps}/>
+                </AnimatePresence>
+            </Hydrate>
         </QueryClientProvider>
+
         // <ThemeProvider enableSystem={true} defaultTheme={"system"} attribute={"class"}>
         //   <AnimatePresence exitBeforeEnter={true}>
         //       <Component {...pageProps}/>
